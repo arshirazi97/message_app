8b3c138cdc3dab837dfcc96e7c5dbf675610d22f
arshirazi97 <90085179+arshirazi97@users.noreply.github.com>
2021-10-09 11:17:36 +0500

    Add message resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_051451) do
+ActiveRecord::Schema.define(version: 2021_10_09_055444) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
